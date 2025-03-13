2c16082cd99ce075f2f532bdf087ffbb4107fb28
viho-kernel <vihari.reddy1802@gmail.com>
2025-03-14 03:28:36 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_FmWBqK9zY', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3bftt7ot58r16m5pnn51ki14aq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
